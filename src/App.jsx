de9bd57d01b6e11b1089f16a75f4174412234616
cy-cheng <cycheng.dev@gmail.com>
2024-05-23 11:39:46 +0800

    change one line in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Subscribe to BrineTaiwan to learn more
       </p>
     </>
   )
