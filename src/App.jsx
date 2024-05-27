29ede98e4c3e6c1b6e9a4705931a10329fb410a8
cy-cheng <cycheng.dev@gmail.com>
2024-05-27 16:34:39 +0800

    Create homepage (header, footer, etc.)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header/Header.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>BrineTaiwan's website</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        if you dont subscribe to BrineTw, I will cry.
+      <Header />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer magna sapien, bibendum mattis eros eget, placerat ornare nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla id maximus libero. Proin tincidunt aliquet diam, vel interdum velit mattis in. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus suscipit dapibus tempus. Aliquam pulvinar consectetur placerat. Mauris sed dui vestibulum, sodales velit non, porttitor lorem. Morbi quis pellentesque augue.
       </p>
+      <p>
+        Quisque faucibus luctus mauris nec ullamcorper. Duis accumsan porttitor ex sit amet semper. Nulla erat tellus, dapibus et consequat sit amet, sagittis sed elit. Fusce enim diam, interdum venenatis tortor vitae, tincidunt euismod arcu. Nunc mattis est ac quam posuere, sit amet semper justo molestie. Ut sit amet sem eget ipsum viverra luctus dapibus vel arcu. Maecenas ullamcorper tempor neque, eu vestibulum sapien mattis at. Integer nisl quam, porttitor bibendum commodo eu, venenatis at leo. Praesent a vestibulum erat, vel convallis tellus.
+      </p>
+
+      <p>Etiam interdum nec nulla ac tempus. Duis a sapien dolor. Phasellus tincidunt tempus tincidunt. Nunc vel metus iaculis, laoreet nisi eu, fringilla velit. Quisque blandit cursus ipsum, id porta elit varius eu. Fusce venenatis sem sit amet orci volutpat, vel hendrerit orci aliquam. Duis hendrerit accumsan consectetur. Quisque placerat ullamcorper tempus.
+      </p>
+      <p>
+        Quisque nec dapibus ligula. Sed vehicula vel odio sit amet tempus. In tristique sed diam a mattis. Cras nec malesuada nisl. Vestibulum euismod magna non nulla vestibulum accumsan. Integer ac gravida enim. Mauris est sem, iaculis non accumsan quis, convallis mollis nunc. Maecenas ut bibendum sapien. Fusce iaculis massa aliquam leo tincidunt, at dignissim eros ultrices. Proin magna magna, commodo vitae fermentum dictum, dapibus dictum dui. Aenean ornare dignissim arcu eu feugiat. Nam et eleifend purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+      </p>
+      <div>
+        <footer>
+          <p>Copyright &copy; {new Date().getFullYear()} Brine</p>
+        </footer>
+      </div>
     </>
   )
 }
