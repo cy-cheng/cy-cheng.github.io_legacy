c3be5be740e4cd67427f62aafb9b072a768618d5
cy-cheng <cycheng.dev@gmail.com>
2024-05-29 14:48:45 +0800

    modify banner layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,22 @@ import Header from './Header/Header.jsx'
 
 function App() {
   return (
-    <>
-      <Header />
+    <div id="page-container">
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer magna sapien, bibendum mattis eros eget, placerat ornare nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla id maximus libero. Proin tincidunt aliquet diam, vel interdum velit mattis in. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus suscipit dapibus tempus. Aliquam pulvinar consectetur placerat. Mauris sed dui vestibulum, sodales velit non, porttitor lorem. Morbi quis pellentesque augue.
-      </p>
-      <p>
-        Quisque faucibus luctus mauris nec ullamcorper. Duis accumsan porttitor ex sit amet semper. Nulla erat tellus, dapibus et consequat sit amet, sagittis sed elit. Fusce enim diam, interdum venenatis tortor vitae, tincidunt euismod arcu. Nunc mattis est ac quam posuere, sit amet semper justo molestie. Ut sit amet sem eget ipsum viverra luctus dapibus vel arcu. Maecenas ullamcorper tempor neque, eu vestibulum sapien mattis at. Integer nisl quam, porttitor bibendum commodo eu, venenatis at leo. Praesent a vestibulum erat, vel convallis tellus.
-      </p>
-
-      <p>Etiam interdum nec nulla ac tempus. Duis a sapien dolor. Phasellus tincidunt tempus tincidunt. Nunc vel metus iaculis, laoreet nisi eu, fringilla velit. Quisque blandit cursus ipsum, id porta elit varius eu. Fusce venenatis sem sit amet orci volutpat, vel hendrerit orci aliquam. Duis hendrerit accumsan consectetur. Quisque placerat ullamcorper tempus.
-      </p>
-      <p>
-        Quisque nec dapibus ligula. Sed vehicula vel odio sit amet tempus. In tristique sed diam a mattis. Cras nec malesuada nisl. Vestibulum euismod magna non nulla vestibulum accumsan. Integer ac gravida enim. Mauris est sem, iaculis non accumsan quis, convallis mollis nunc. Maecenas ut bibendum sapien. Fusce iaculis massa aliquam leo tincidunt, at dignissim eros ultrices. Proin magna magna, commodo vitae fermentum dictum, dapibus dictum dui. Aenean ornare dignissim arcu eu feugiat. Nam et eleifend purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-      </p>
-      <div>
-        <footer>
-          <p>Copyright &copy; {new Date().getFullYear()} Brine</p>
-        </footer>
+      <div id="content-section">
+        <Header />
+        <div class="centered">
+          <div>
+            <h2>Hello World!</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat vehicula pellentesque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam consectetur diam imperdiet lorem faucibus congue. Vestibulum gravida ultricies dolor sit amet pellentesque. Curabitur imperdiet ultrices turpis, eu ultrices dolor rhoncus id. Suspendisse viverra finibus leo, ac pretium est eleifend eget. Quisque faucibus orci non erat accumsan commodo. Aenean ac mauris vestibulum, congue turpis non, iaculis diam. Quisque vitae tristique nibh. Cras consequat dignissim ex, eget interdum turpis accumsan sed. Sed non turpis in ligula congue tempus ut sed leo.</p>
+          </div>
+        </div>
       </div>
-    </>
+
+      <footer>
+        <p>Copyright &copy; {new Date().getFullYear()} Brine</p>
+      </footer>
+    </div>
   )
 }
 
